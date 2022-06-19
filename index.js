f8e626b4387b0338cfdc497e7522f0292a64ebec
David Munoz <jdavid_9502@outlook.com>
2022-06-18 20:29:17 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const cors = require('cors');
+const { default: axios } = require('axios');
+const API_URL = 'http://ddragon.leagueoflegends.com/cdn/12.11.1/data/en_US/champion.json';
+const PORT = 8080;
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.options('*',cors());
+
+app.listen(PORT, () => {
+    // console.log('Aqui merengues');
+
+    // axios.get(API_URL + 'Ezreal.json', {
+    // }).then((res) => {
+    //     console.log(res);
+    // })
+    // .catch((reason) => {
+    //     console.log(reason);
+    // });
+});
+
+// app.get('/aqui-merengues', (req, res) => {
+//     res.send('Simon');
+// });
+
+app.get('/get-random-champ', (req, res) => {
+    getChamp().then((randomChamp) => {
+        res.send(randomChamp)
+    });
+});
+
+function getChamp(champName) {
+    return new Promise((resolve, reject) => {
+        axios
+            .get(API_URL)
+            .then((res) => {
+                let champ = getRandomChamp(res.data.data);
+                resolve(champ);
+            })
+            .catch((reason) => {
+                reject(reason);
+            })
+    });
+}
+
+function getRandomChamp(champList) {
+    let names = Object.keys(champList);
+    let random = names[Math.floor(Math.random() * names.length)];
+    // console.log(random);
+
+    return random;
+
+
+    // console.log(names);
+}
+
+
+
