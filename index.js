5355bc2454bb8b19f6d068ea4eac87c5bbf1603d
David Munoz <jdavid_9502@outlook.com>
2022-06-18 21:57:39 -0500

    Connectionto discord bot

--- index.js
+++ index.js
@@ -1,39 +1,53 @@
 const express = require('express');
 const cors = require('cors');
+const Discord = require('discord.js');
 const { default: axios } = require('axios');
 const API_URL = 'http://ddragon.leagueoflegends.com/cdn/12.11.1/data/en_US/champion.json';
 const app = express();
 
-// process.env.PORT = 8080;
-// const PORT = process.env.PORT;
+let test = {};
+let i = 0;
 
 app.use(express.json());
 app.use(cors());
 app.options('*',cors());
 
+require('dotenv').config(); //initialize dotenv
+
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"]}); //create new client
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', (message) => {
+    if (message.channelId !== "843700429450641419" || message.author.id === "987880205873999932")
+    return;
+    console.log(i++);
+
+    if (isCommand(message.content)) {
+        getChamp().then((name) => {
+            message.channel.send(name);
+        });
+    }
+
+    test[message.author.id] = true;
+    console.log(message);
+});
+
+function isCommand(text) {
+    return text[0] === '!';
+}
+
+
+//make sure this line is the last line
+client.login(process.env.DISCORD_TOKEN); //login bot using token
+
 app.listen(process.env.PORT || 5000, () => {
-    // console.log('Aqui merengues');
-
-    // axios.get(API_URL + 'Ezreal.json', {
-    // }).then((res) => {
-    //     console.log(res);
-    // })
-    // .catch((reason) => {
-    //     console.log(reason);
-    // });
+    console.log('Aqui merengues');
 });
 
-// app.get('/aqui-merengues', (req, res) => {
-//     res.send('Simon');
-// });
-
-app.get('/get-random-champ', (req, res) => {
-    getChamp().then((randomChamp) => {
-        res.send(randomChamp)
-    });
-});
-
-function getChamp(champName) {
+function getChamp() {
     return new Promise((resolve, reject) => {
         axios
             .get(API_URL)
@@ -50,12 +64,7 @@ function getChamp(champName) {
 function getRandomChamp(champList) {
     let names = Object.keys(champList);
     let random = names[Math.floor(Math.random() * names.length)];
-    // console.log(random);
-
     return random;
-
-
-    // console.log(names);
 }
 
 
